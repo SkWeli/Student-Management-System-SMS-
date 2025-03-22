bfe1d5c076732ba2fc82e499c72b7c966ee2603a
Senuda Weliwatta <senudaweli@gmail.com>
2025-03-22 20:44:53 +0530

    header 90% completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from "react-bootstrap";
 import { BsBoxArrowRight } from "react-icons/bs";
+import logo from '../../assets/KDU_logo.png';
 
 const Header = () => {
   return (
@@ -16,17 +17,38 @@ const Header = () => {
       </div>
 
       {/* Navigation Bar */}
-      <nav className="navbar navbar-expand-lg" style={{ background: 'linear-gradient(to right, #003087, #005EB8)' }}>
-        <Container className="d-flex align-items-center">
+      <nav className="navbar navbar-expand-lg" 
+      style={{ 
+        background: 'linear-gradient(to right, #003087,rgb(21, 83, 200),rgb(12, 131, 243))',
+        borderRadius: '0 0 15px 15px',
+         }}>
+        <Container className="d-flex align-items-center justify-content-start" style={{ gap: '10px', padding: 0, margin: 0 }}>
           <img
-            src="C:/Users/senud/sms-frontend/src/assets/KDU_logo.png" 
+            src= {logo}
             alt="Logo"
-            style={{ width: '50px', height: '50px', marginRight: '15px' }}
+            style={{ width: '120px', height: '120px', marginLeft: '120px', marginRight: '50px' }}
           />
-          <span className="navbar-brand text-white" style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
-            DEPARTMENT OF SOFTWARE ENGINEERING<br />
-            FACULTY OF COMPUTING<br />
-            GENERAL SIR JOHN KOTELAWALA DEFENCE UNIVERSITY
+          <span
+            className="text-white"
+            style={{
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              lineHeight: '1.2',
+              margin: 0,
+              padding: 0
+            }}
+          >
+            <span style={{ fontSize: '2rem', fontWeight: 'bold' }}>
+              Department of Software Engineering
+            </span>
+            <br />
+            <span style={{ fontSize: '1.5rem', fontWeight: 'normal' }}>
+              Faculty Of Computing
+            </span>
+            <br />
+            <span style={{ fontSize: '1.3rem', fontStyle: 'italic' }}>
+              General Sir John Kotelawala Defence University
+            </span>
           </span>
         </Container>
       </nav>
